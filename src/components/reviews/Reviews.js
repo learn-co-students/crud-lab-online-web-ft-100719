88b4dee59529323549ccfd3247697dca4ff33281
Sarah Glotzbach <sarah.glotzbach@gmail.com>
2020-03-09 10:04:11 -0400

    Done.

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -2,11 +2,20 @@ import React, { Component } from 'react';
 import Review from './Review';
 
 class Reviews extends Component {
+
   render() {
+
+    const filteredReviews = this.props.reviews.filter(review => review.restaurantId === this.props.restaurantId)
+    const reviews = filteredReviews.map((review)=> {
+      return <Review key={review.id} review={review} deleteReview={this.props.deleteReview} />
+    })
+
     return (
-      <ul>
-        Reviews
-      </ul>
+      <div>
+        <ul>
+          {reviews}
+        </ul>
+      </div> 
     );
   }
 };
